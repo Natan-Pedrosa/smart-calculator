dfe6567f6fb5a1c57ab1bf0a777b4702d96489ab
Natan Pedrosa <natanpedrosa123@gmail.com>
2024-02-08 13:17:48 -0300

    Add function definePlusOrMinus that get a sequence of operators plus and/or minus and define the result.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 package org.example
 class Calculator{
 
-    fun sum(values: List<Int>): Int{
+    fun sum(vararg values: Int): Int{
         var result = 0
 
         for (value in values){
@@ -12,7 +12,43 @@ class Calculator{
         return result
     }
 
+    fun subtract(vararg values: Int): Int {
+        var result = 0
+
+        for (value in values){
+
+            result -= value
+        }
+
+        return result
+    }
+
+    fun definePlusOrMinus(symbols: List<String>): String{
+        var result = ""
+
+        if (symbols.isEmpty())
+            return result
+
+        result = symbols[0]
+
+        for (index in 1 .. symbols.lastIndex){
+            val prev = result
+            val next = symbols[index]
+
+            val tempResult = prev + next
+            result = when (tempResult) {
+                "++" -> "+"
+                "-+" -> "-"
+                "+-" -> "-"
+                "--" -> "+"
+                else -> ""
+            }
+        }
+
+        return result
+    }
 }
+
 
 fun main() {
     val calculator = Calculator()
@@ -25,9 +61,32 @@ fun main() {
             input.contains("/help")  -> println("The program calculates the sum of numbers")
             input.contains("/exit") -> break
             else -> {
-                val values = input.split(" ").map { it.toInt() }
+                val values = input.split(" ")
 
-                println(calculator.sum(values))
+                if(values.size == 1) {
+                    println(values[0])
+                    continue
+                }
+
+                var result = 0
+
+                for (index in 1 .. values.lastIndex step 2){
+                    val prev = result
+                    val next = values[index + 1].toInt()
+                    val symbol = values[index]
+
+                    val numberPlus = symbol.filter { it == '+' }.length
+                    val numberMinus = symbol.filter { it == '-' }.length
+
+                    val isPlus = numberPlus >= numberMinus
+
+                    result = when{
+                        isPlus -> calculator.sum(prev, next)
+                        else -> calculator.subtract(prev, next)
+                    }
+                }
+
+                println(result)
             }
         }
     }
