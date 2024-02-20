9094dcfd9de36ae4d57872e9dcfe927c9d3d77d3
Natan Pedrosa <natanpedrosa123@gmail.com>
2024-02-20 11:30:49 -0300

    Add convertInFixToPostFixExpression and calculatePostFixExpression

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 ```
-> 8 + 7 - 4
+> 8 * 3 + 12 * (4 - 2)
+48
+> 2 - 2 + 3
+3
+> 4 * (2 + 3
+Invalid expression
+> -10
+-10
+> a=4
+> b=5
+> c=6
+> a*2+b*3+c*(2+3)
+53
+> 1 +++ 2 * 3 -- 4
 11
-> abc
+> 3 *** 5
 Invalid expression
-> 123+
+> 4+3)
 Invalid expression
-> +15
-15
-> 18 22
-Invalid expression
->
-> -22
--22
-> 22-
-Invalid expression
-> /go
+> /command
 Unknown command
 > /exit
 Bye!
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
+import kotlin.math.pow
 
 class Calculator{
+
+    val memory = mutableMapOf<String, Int>()
 
     fun sum(vararg values: Int): Int{
         var result = 0
@@ -49,8 +52,43 @@ class Calculator{
 
         return result
     }
+
+    fun operation(firstValue: Int, secondValue: Int, operator: String): Int{
+        return when(operator){
+            "+" -> sum(secondValue, firstValue)
+            "-" -> subtract(secondValue, firstValue)
+            "*" -> firstValue * secondValue
+            "/" -> secondValue / firstValue
+            "^" -> secondValue.toDouble().pow(firstValue.toDouble()).toInt()
+            else -> 0
+        }
+    }
+
+    fun calculatePostFixExpression(expressionPostFix: List<String>): Int{
+        val stack = mutableListOf<Int>()
+        val operators = listOf("/", "*", "^", "+", "-")
+
+        for (value in expressionPostFix){
+            when{
+                value.toIntOrNull() != null -> stack.add(value.toInt())
+                memory.containsKey(value) -> stack.add(memory[value]!!.toInt())
+                value in operators -> {
+                    val firstValue = stack.removeLast()
+                    val secondValue = stack.removeLast()
+
+                    val result = operation(firstValue, secondValue, value)
+                    stack.add(result)
+                }
+            }
+        }
+
+        return stack.last()
+    }
 }
 
+fun isDigit(value: String): Boolean{
+    return value.toIntOrNull() != null
+}
 
 fun main() {
     val calculator = Calculator()
@@ -64,11 +102,53 @@ fun main() {
                     "Also support unary and binary plus/minus operator")
             input.contains("/exit") -> break
             input.contains("/") -> println("Unknown command")
+            input.contains("=") -> {
+
+                val array = input.filter{it != ' '}.split("=")
+
+                if(array.size > 2){
+                    println("Invalid assignment")
+                    continue
+                }
+
+                val key = array[0]
+                val value = array[1]
+
+                if(isNoBasicLatinLetter(key)){
+                    println("Invalid identifier")
+                    continue
+                }
+
+                if(isDigit(value))
+                    calculator.memory[key] = value.toInt()
+                else{
+                    if(isNoBasicLatinLetter(value)) {
+                        println("Invalid identifier")
+                        continue
+                    }
+
+                    if(calculator.memory.contains(value))
+                            calculator.memory[key] = calculator.memory.getOrDefault(value, 0)
+                        else
+                            println("Unknown variable")
+                    }
+            }
             else -> {
                 val values = input.split(" ").filter { it != "" }
 
                 if(values.size == 1) {
                   when{
+                      input.any{ it.isLetter()} -> {
+                          if (isNoBasicLatinLetter(input)){
+                              println("Invalid identifier")
+                              continue
+                          }
+
+                          if(calculator.memory.contains(input))
+                              println(calculator.memory[input])
+                          else
+                              println("Unknown variable")
+                      }
                       input[0] == '+' -> println(input.subSequence(1, input.length))
                       !input[input.length - 1].isDigit() -> println("Invalid expression")
                       else -> println(input)
@@ -77,20 +157,10 @@ fun main() {
                 }
 
                 try {
-                    var result = values[0].toInt()
 
-                    for (index in 1..values.lastIndex step 2) {
-                        val prev = result
-                        val symbol = calculator.definePlusOrMinus(values[index])
-                        val next = values[index + 1].toInt()
+                    val postFixExpression = convertInFixToPostFixExpression(values)
 
-                        result = when {
-                            symbol == "+" -> calculator.sum(prev, next)
-                            else -> calculator.subtract(prev, next)
-                        }
-                    }
-
-                    println(result)
+                    println(calculator.calculatePostFixExpression(postFixExpression))
                 } catch (e: Exception){
                     println("Invalid expression")
                 }
@@ -99,4 +169,48 @@ fun main() {
     }
 
     println("Bye!")
+}
+
+fun isNoBasicLatinLetter(key: String): Boolean {
+    return key.any { (it.code !in 65 .. 90) && (it.code !in 97 .. 122)}
+}
+
+fun isDigitOrVariable(value: String): Boolean{
+    return value.any { it.isDigit() || it.isLetter() }
+}
+
+fun isIncomingOperatorWithHigherPrecedence(incomingOperator: String, topStackOperator: String): Boolean{
+    val multipleOrDivision = mutableListOf("*", "/")
+
+    return  when(incomingOperator){
+        "^" -> incomingOperator != topStackOperator
+        in multipleOrDivision -> topStackOperator !in multipleOrDivision
+        else ->  false
+    }
+}
+fun convertInFixToPostFixExpression(expression: List<String>): MutableList<String>{
+    val stack = mutableListOf<String>()
+    val postFixExpression = mutableListOf<String>()
+
+    for(value in expression){
+        when{
+            isDigitOrVariable(value) -> postFixExpression.add(value)
+            stack.isEmpty() || stack.last() == "(" -> stack.add(value)
+            isIncomingOperatorWithHigherPrecedence(value, stack.last()) -> stack.add(value)
+            value == stack.last() || !isIncomingOperatorWithHigherPrecedence(value, stack.last()) ->
+                postFixExpression.add(stack.removeLast())
+            value == "(" -> stack.add(value)
+            value == ")" -> {
+                while (stack.last() != "(") {
+                    postFixExpression.add(stack.removeLast())
+                }
+                stack.removeLast()
+            }
+        }
+    }
+
+    while (stack.isNotEmpty())
+        postFixExpression.add(stack.removeLast())
+
+    return postFixExpression
 }
